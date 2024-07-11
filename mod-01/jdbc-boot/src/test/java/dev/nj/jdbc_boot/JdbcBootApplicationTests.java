175156825f04b7b0ff49bbed2db9c333961e05fb
eucarizan <eucarizan@gmail.com>
2024-07-11 16:21:38 +0800

    feat: module 1, write integration testing code

--- mod-01/jdbc-boot/src/test/java/dev/nj/jdbc_boot/JdbcBootApplicationTests.java
+++ mod-01/jdbc-boot/src/test/java/dev/nj/jdbc_boot/JdbcBootApplicationTests.java
@@ -1,13 +1,24 @@
 package dev.nj.jdbc_boot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JdbcBootApplicationTests {
+	public static final String QUERY = "SELECT count(*) FROM T_ACCOUNT";
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Test
-	void contextLoads() {
+	void testNumberOfAccount() {
+		long count = jdbcTemplate.queryForObject(QUERY, Long.class);
+
+		assertThat(count).isEqualTo(21L);
 	}
 
 }
