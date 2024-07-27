725204f5700bfaedf1e8c2a9aed3e09c20789801
eucarizan <eucarizan@gmail.com>
2024-07-27 11:33:20 +0800

    feat: module 7, todo-04, configure authorization

--- mod-07/42-security-rest/src/main/java/accounts/RestWsApplication.java
+++ mod-07/42-security-rest/src/main/java/accounts/RestWsApplication.java
@@ -1,8 +1,10 @@
 package accounts;
 
+import config.RestSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(RestSecurityConfig.class)
