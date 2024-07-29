98d5670cca6aebb96abb9055a231a38bbba738a8
eucarizan <eucarizan@gmail.com>
2024-07-29 21:01:52 +0800

    feat: module 7, todo-06a, perform security testing against mvc layer

--- mod-07/42-security-rest/src/test/java/accounts/web/AccountControllerTests.java
+++ mod-07/42-security-rest/src/test/java/accounts/web/AccountControllerTests.java
@@ -27,11 +27,6 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-// TODO-06a: Perform security testing against MVC layer
-// - Take some time to understand what each test is for
-// - Remove @Disabled annotation from each test and run it
-// - Make sure all tests pass
-
 @WebMvcTest(AccountController.class)
 @ContextConfiguration(classes = {RestWsApplication.class, RestSecurityConfig.class})
 public class AccountControllerTests {
@@ -46,19 +41,15 @@ public class AccountControllerTests {
     private AccountService accountService;
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"INVALID"})
     void accountSummary_with_invalid_role_should_return_403() throws Exception {
-
         mockMvc.perform(get("/accounts"))
                .andExpect(status().isForbidden());
     }
 
     @Test
-    @Disabled
-    @WithMockUser( roles = {"USER"})
+    @WithMockUser(roles = {"USER"})
     public void accountDetails_with_USER_role_should_return_200() throws Exception {
-
         // arrange
         given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
 
@@ -70,14 +61,11 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(username = "user", password = "user")
     public void accountDetails_with_user_credentials_should_return_200() throws Exception {
-
         // arrange
         given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
 
@@ -89,14 +77,11 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(username = "admin", password = "admin")
     public void accountDetails_with_admin_credentials_should_return_200() throws Exception {
-
         // arrange
         given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
 
@@ -108,14 +93,11 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(username = "superadmin", password = "superadmin")
     public void accountDetails_with_superadmin_credentials_should_return_200() throws Exception {
-
         // arrange
         given(accountManager.getAccount(0L)).willReturn(new Account("1234567890", "John Doe"));
 
@@ -128,14 +110,11 @@ public class AccountControllerTests {
 
         // verify
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"USER"})
     public void accountDetailsFail_test_with_USER_role_should_proceed_successfully() throws Exception {
-
         given(accountManager.getAccount(any(Long.class)))
                 .willThrow(new IllegalArgumentException("No such account with id " + 0L));
 
@@ -143,14 +122,11 @@ public class AccountControllerTests {
                .andExpect(status().isNotFound());
 
         verify(accountManager).getAccount(any(Long.class));
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"ADMIN"})
     public void accountSummary_with_ADMIN_role_should_return_200() throws Exception {
-
         List<Account> testAccounts = Arrays.asList(new Account("123456789", "John Doe"));
         given(accountManager.getAllAccounts()).willReturn(testAccounts);
 
@@ -161,14 +137,11 @@ public class AccountControllerTests {
                .andExpect(jsonPath("$..name").value("John Doe"));
 
         verify(accountManager).getAllAccounts();
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"ADMIN", "SUPERADMIN"})
     public void createAccount_with_ADMIN_or_SUPERADMIN_role_should_return_201() throws Exception {
-
         Account testAccount = new Account("1234512345", "Mary Jones");
         testAccount.setEntityId(21L);
         given(accountManager.save(any(Account.class))).willReturn(testAccount);
@@ -180,7 +153,6 @@ public class AccountControllerTests {
                .andExpect(header().string("Location", "http://localhost/accounts/21"));
 
         verify(accountManager).save(any(Account.class));
-
     }
 
     // TODO-06b: Write a test that verifies that a user with "USER" role
@@ -191,6 +163,7 @@ public class AccountControllerTests {
     //    this testing because security failure will prevent
     //    calling a method of a dependency.)
     @Test
+    @Disabled
     public void createAccount_with_USER_role_should_return_403() throws Exception {
 
 
@@ -198,10 +171,8 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"SUPERADMIN"})
     public void getBeneficiary_with_SUPERADMIN_role_should_return_200() throws Exception {
-
         Account account = new Account("1234567890", "John Doe");
         account.addBeneficiary("Corgan", new Percentage(0.1));
         given(accountManager.getAccount(0L)).willReturn(account);
@@ -216,29 +187,23 @@ public class AccountControllerTests {
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"ADMIN", "SUPERADMIN"})
     public void addBeneficiary_with_ADMIN_or_SUPERADMIN_role_should_return_201() throws Exception {
-
         mockMvc.perform(post("/accounts/{entityId}/beneficiaries", 0L).content("Kate"))
                .andExpect(status().isCreated())
                .andExpect(header().string("Location", "http://localhost/accounts/0/beneficiaries/Kate"));
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"USER"})
     public void addBeneficiary_with_USER_role_should_return_403() throws Exception {
-
         mockMvc.perform(post("/accounts/{entityId}/beneficiaries", 0L).content("Kate"))
                .andExpect(status().isForbidden());
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"SUPERADMIN"})
     public void removeBeneficiary_with_SUPERADMIN_role_should_return_204() throws Exception {
-
         Account account = new Account("1234567890", "John Doe");
         account.addBeneficiary("Corgan", new Percentage(0.1));
         given(accountManager.getAccount(0L)).willReturn(account);
@@ -247,25 +212,20 @@ public class AccountControllerTests {
                .andExpect(status().isNoContent());
 
         verify(accountManager).getAccount(0L);
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"USER", "ADMIN"})
     public void removeBeneficiary_with_USER_or_ADMIN_role_should_return_403() throws Exception {
-
         Account account = new Account("1234567890", "John Doe");
         account.addBeneficiary("Corgan", new Percentage(0.1));
         given(accountManager.getAccount(0L)).willReturn(account);
 
         mockMvc.perform(delete("/accounts/{entityId}/beneficiaries/{name}", 0L, "Corgan"))
                .andExpect(status().isForbidden());
-
     }
 
     @Test
-    @Disabled
     @WithMockUser(roles = {"SUPERADMIN"})
     public void removeBeneficiaryFail_test_with_SUPERADMIN_role_should_proceed_successfully() throws Exception {
         Account account = new Account("1234567890", "John Doe");
