519575f247096c7408a07101d35a7453797dc651
eucarizan <eucarizan@gmail.com>
2024-08-02 00:03:50 +0800

    feat: module 8, todo-25, experiment with healthindicator

--- mod-08/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ mod-08/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -2,6 +2,7 @@ package accounts.web;
 
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
 import org.springframework.stereotype.Component;
 import rewards.internal.restaurant.RestaurantRepository;
 
@@ -19,26 +20,11 @@ public class RestaurantHealthCheck implements HealthIndicator {
         Long restaurantCount = restaurantRepository.getRestaurantCount();
         if (restaurantCount > 0) {
             return Health.up()
+                    .withDetail("restaurantCount", restaurantCount)
                     .build();
         } else {
-            return Health.down().build();
+            return Health.status("NO_RESTAURANTS").build();
         }
     }
 
 }
-
-
-/**
- * TODO-25 (Optional): Experiment with HealthIndicator above
- * - Change "spring.sql.init.data-locations" property in the
- *   "application.properties" file back to use "data-no-restaurants.sql"
- * - Include the restaurant count as extra detail when DOWN state.
- * - Instead of returning DOWN when there are no restaurants,
- *   define and use a custom status called NO_RESTAURANTS.
- * - When there are no restaurants in the DB, what top-level status
- *   is returned for the "application" health group?
- * - Set "management.endpoint.health.group.application.status.order" property
- *   in the "application.properties" file so that NO_RESTAURANTS
- *   gets displayed as top-level status for the "application" health group.
- * - Restart the application and verify the result.
- */
